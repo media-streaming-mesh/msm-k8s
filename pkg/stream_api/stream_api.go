1825807d988d4304c4cda54ef242501dfb98731d
Chien <chiennguyen44@gmail.com>
2023-07-13 05:00:52 -0700

    Update stream_api for CRD get/update/delete/watch, add backward compatibility

--- pkg/stream_api/stream_api.go
+++ pkg/stream_api/stream_api.go
@@ -4,14 +4,25 @@ import (
 	"context"
 	"fmt"
 	mediastreamsv1 "github.com/media-streaming-mesh/msm-k8s/api/v1"
+	"github.com/media-streaming-mesh/msm-k8s/pkg/model"
 	"github.com/sirupsen/logrus"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
 	utilruntime "k8s.io/apimachinery/pkg/util/runtime"
+	"k8s.io/apimachinery/pkg/watch"
 	clientgoscheme "k8s.io/client-go/kubernetes/scheme"
 	"k8s.io/client-go/rest"
 	"log"
 	"sigs.k8s.io/controller-runtime/pkg/client"
+	"strings"
 	"time"
+)
+
+const (
+	CRDPlural   string = "streamdata"
+	CRDGroup    string = "mediastreams.media-streaming-mesh.io"
+	CRDVersion  string = "v1"
+	FullCRDName string = CRDPlural + "." + CRDGroup
 )
 
 type StreamAPI struct {
@@ -56,14 +67,102 @@ func (s *StreamAPI) List() (*mediastreamsv1.StreamdataList, error) {
 	return result, err
 }
 
-func (s *StreamAPI) Create(data *mediastreamsv1.Streamdata) error {
+func (s *StreamAPI) Get(name string) (*mediastreamsv1.Streamdata, error) {
 	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
-	err := s.crdClient.Create(ctx, data)
+	result := &mediastreamsv1.Streamdata{}
+
+	err := s.crdClient.Get(ctx, client.ObjectKey{Namespace: "default", Name: name}, result)
+	return result, err
+}
+
+// TODO: use mediastreamsv1.Streamdata when refactor msm-cp and msm-nc to mediastreamsv1.Streamdata
+func (s *StreamAPI) Create(data model.StreamData) error {
+	crdData := s.modelObjToCrdObj(data)
+	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
+	err := s.crdClient.Create(ctx, crdData)
 	return err
 }
 
-func (s *StreamAPI) Update(data *mediastreamsv1.Streamdata) error {
+// TODO: use mediastreamsv1.Streamdata when refactor msm-cp and msm-nc to mediastreamsv1.Streamdata
+func (s *StreamAPI) Update(data model.StreamData) error {
+	crdData := s.modelObjToCrdObj(data)
 	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
-	err := s.crdClient.Update(ctx, data)
+	err := s.crdClient.Update(ctx, crdData)
 	return err
+}
+
+// TODO: use mediastreamsv1.Streamdata when refactor msm-cp and msm-nc to mediastreamsv1.Streamdata
+func (s *StreamAPI) Delete(data model.StreamData) error {
+	crdData := s.modelObjToCrdObj(data)
+	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
+	err := s.crdClient.Delete(ctx, crdData)
+	return err
+}
+
+// TODO: use mediastreamsv1.Streamdata when refactor msm-cp and msm-nc to mediastreamsv1.Streamdata
+func (s *StreamAPI) WatchStreams(dataChan chan<- model.StreamData) {
+	s.log("Start WATCH for CRD streamData")
+	result := &mediastreamsv1.StreamdataList{}
+	watchInterface, err := s.crdClient.Watch(context.Background(), result)
+	if err != nil {
+		s.log("Failed watch CRD streamData with error %v", err)
+	}
+
+	select {
+	case event := <-watchInterface.ResultChan():
+		s.log("Watch event %v result %v", event.Type, event.Object)
+		crdData := event.Object.(*mediastreamsv1.Streamdata)
+		dataChan <- s.crObjToModelObj(crdData)
+
+		if event.Type == watch.Added {
+			s.log("Received Add event")
+		}
+
+	}
+}
+
+func (s *StreamAPI) crObjToModelObj(crdData *mediastreamsv1.Streamdata) model.StreamData {
+	var streamState model.StreamState
+
+	if strings.ToLower(crdData.Spec.StreamState) == "create" {
+		streamState = model.Create
+	} else if strings.ToLower(crdData.Spec.StreamState) == "play" {
+		streamState = model.Play
+	} else if strings.ToLower(crdData.Spec.StreamState) == "teardown" {
+		streamState = model.Teardown
+	}
+
+	return model.StreamData{
+		StubIp:      crdData.Spec.StubIp,
+		ServerIp:    crdData.Spec.ServerIp,
+		ClientIp:    crdData.Spec.ClientIp,
+		ClientPorts: []uint32{uint32(crdData.Spec.ClientPort)},
+		ServerPorts: []uint32{uint32(crdData.Spec.ServerPort)},
+		StreamState: streamState,
+	}
+}
+
+func (s *StreamAPI) modelObjToCrdObj(data model.StreamData) *mediastreamsv1.Streamdata {
+	return &mediastreamsv1.Streamdata{
+		TypeMeta: metav1.TypeMeta{
+			Kind:       "Streamdata",
+			APIVersion: CRDGroup + "/" + CRDVersion,
+		},
+		ObjectMeta: metav1.ObjectMeta{
+			Name: FullCRDName,
+		},
+		Spec: mediastreamsv1.StreamdataSpec{
+			StubIp:     data.StubIp,
+			ServerIp:   data.ServerIp,
+			ClientIp:   data.ClientIp,
+			ServerPort: int(data.ServerPorts[0]),
+			ClientPort: int(data.ClientPorts[0]),
+			NodeID:     "",
+		},
+		Status: mediastreamsv1.StreamdataStatus{
+			Status:       "PENDING",
+			Reason:       "SETUP FROM APP",
+			StreamStatus: "CREATE",
+		},
+	}
 }
